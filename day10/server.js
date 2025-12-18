b978d5a67cfd6eb7e0fd19ab177b22ba876cac44
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-12-18 10:32:26 +0530

    fetch api

---
+++ day10/server.js
@@ -0,0 +1,11 @@
+function getAnimalData(){
+    //ten 10 random people
+    fetch("https://randomuser.me/api/?results=10")
+    .then((res)=>{
+        return res.json();
+    })
+    .then((data)=>{
+        console.log(data);
+    
+    })
+}
